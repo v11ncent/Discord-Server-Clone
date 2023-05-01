f42c6109bc0d5747b30285a3530d26d141da20a8
Vince <45806875+vince1444@users.noreply.github.com>
2023-04-30 23:23:15 -0400

    Finished server navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
-const statuses = document.querySelectorAll(".status[data-status=game]");
+const serversNavigation = document.querySelector(".servers__navigation");
 
-// cool!
-console.log(statuses);
+/*
+ * Woo, I love event delegation! 🙂
+ */
+
+serversNavigation.addEventListener("click", (event) => {
+  if (event.target.tagName.toLowerCase() !== "button") return;
+
+  // Grab current active server and set aria-expanded to false
+
+  const newServer = event.target;
+  const currentServer = document.querySelector(
+    ".servers__navigation button[aria-expanded=true]"
+  );
+
+  currentServer && currentServer.setAttribute("aria-expanded", false);
+  newServer.setAttribute("aria-expanded", true);
+});
